bc1d2c7d7082c061e33854f933537c3bb10eb7e4
Arthur Endlein <endarthur@gmail.com>
2012-06-27 16:54:18 -0300

    Did a bit of cleanup, removing the unused function that made the old encoding, eons ago. Like last year. Told python about the file encoding. Hey, this seems to be py3 compatible, by the way. Gotta check that.

--- cim.py
+++ cim.py
@@ -1,22 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # A International World Map encoder - from geographic coordinates
 # May someday accept UTM as well, but tonight it will be simpler
 # OO mode, hopefully
-
 from string import uppercase as zone
-
-
-def find_scale(lat, lon):
-    five_hundred = (('V', 'X'), ('Y', 'Z')) # 2 by 3
-    two_hundred_fifty = (('A', 'B'), ('C', 'D')) # 1 by 1.5
-    one_hundred = (('I', 'II', 'III'), ('IV', 'V', 'VI')) # 0.5 by 0.5
-    fifty = (('1', '2'), ('3', '4')) # 0.25 by 0.25
-    twenty_five = (('NO', 'NE'), ('SO', 'SE')) # 0.125 by 0.125
-    scale = five_hundred[int(lat % 4 / 2)][int(lon % 6 / 3)]
-    scale += '-' + two_hundred_fifty[int(lat % 2 / 1)][int(lon % 3 / 1.5)]
-    scale += '-' + one_hundred[int(lat % 1 / 0.5)][int(lon % 1.5 / 0.5)]
-    scale += '-' + fifty[int(lat % 0.5 / 0.25)][int(lon % 0.5 / 0.25)]
-    scale += '-' + twenty_five[int(lat % 0.25 / 0.125)][int( lon % 0.25 / 0.125)]
-    return scale
 
 class CIMcode(object):
     def __init__(self, lat, lon):
@@ -36,6 +23,7 @@ class CIMcode(object):
         """ Just cat everything in the max scale defined by the CIM code. """
         return "%s%s-%s-%s" % (self.hemisphere, self.zone, self.fuse, "-".join(self.scales))
     def __getitem__(self, scale):
+        """ Return a list with the scale codes up to the scale requested. """
         return self.scales[0:self.scalesindex[scale] + 1]
             
             
