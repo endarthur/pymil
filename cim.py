d021b120f44d0c79f6649f4fb6f8e6f8d3ab8c5b
Arthur Endlein <endarthur@gmail.com>
2012-06-27 16:40:58 -0300

    Reverted the change from using a Dict in internal repr, as I remembered that a Dict values() order cannot be guaranteed. Made a __get__ method that returns the scale code up to the scale used as key. May not be that logical, considering that it returns a list. Well, maybe I should include the Hemisphere, zone and fuse in that, so as the output will be usable directly without adding other parts. Honestly, I think it is not needed. We'll see. Wish I had Internet...

--- cim.py
+++ cim.py
@@ -20,22 +20,22 @@ def find_scale(lat, lon):
 
 class CIMcode(object):
     def __init__(self, lat, lon):
-        """ Create the CIMcode object, from latitude and longitude in decimal degrees. """
+        """ Create the CIMcode object, from latitude and longitude in decimal degrees. 
+        In the future, hopefully, it will be possible to use different syntaxes for lat and lon."""
         self.coordinates = (lat, lon)
-        self.scales = {"500k":(('V', 'X'), ('Y', 'Z'))[int(lat % 4 / 2)][int(lon % 6 / 3)], # 2 by 3
-"250k":(('A', 'B'), ('C', 'D'))[int(lat % 2 / 1)][int(lon % 3 / 1.5)], # 1 by 1.5
-"100k":(('I', 'II', 'III'), ('IV', 'V', 'VI'))[int(lat % 1 / 0.5)][int(lon % 1.5 / 0.5)], # 0.5 by 0.5
-"50k":(('1', '2'), ('3', '4'))[int(lat % 0.5 / 0.25)][int(lon % 0.5 / 0.25)], # 0.25 by 0.25
-"25k":(('NO', 'NE'), ('SO', 'SE'))[int(lat % 0.25 / 0.125)][int( lon % 0.25 / 0.125)]} # 0.125 by 0.125
+        self.scales = [(('V', 'X'), ('Y', 'Z'))[int(lat % 4 / 2)][int(lon % 6 / 3)], # 2 by 3
+(('A', 'B'), ('C', 'D'))[int(lat % 2 / 1)][int(lon % 3 / 1.5)], # 1 by 1.5
+(('I', 'II', 'III'), ('IV', 'V', 'VI'))[int(lat % 1 / 0.5)][int(lon % 1.5 / 0.5)], # 0.5 by 0.5
+(('1', '2'), ('3', '4'))[int(lat % 0.5 / 0.25)][int(lon % 0.5 / 0.25)], # 0.25 by 0.25
+(('NO', 'NE'), ('SO', 'SE'))[int(lat % 0.25 / 0.125)][int( lon % 0.25 / 0.125)]] # 0.125 by 0.125
         self.hemisphere = (lat > 0) and "N" or "S"
         self.zone = zone[int(abs(lat))/4]
         self.fuse = str((180 + int(lon))/6 + 2)
-        #~ scale = five_hundred[int(lat % 4 / 2)][int(lon % 6 / 3)]
-        #~ scale += '-' + two_hundred_fifty[int(lat % 2 / 1)][int(lon % 3 / 1.5)]
-        #~ scale += '-' + one_hundred[int(lat % 1 / 0.5)][int(lon % 1.5 / 0.5)]
-        #~ scale += '-' + fifty[int(lat % 0.5 / 0.25)][int(lon % 0.5 / 0.25)]
-        #~ scale += '-' + twenty_five[int(lat % 0.25 / 0.125)][int( lon % 0.25 / 0.125)]
     def __repr__(self):
-        return "%s%s-%s-%s" % (self.hemisphere, self.zone, self.fuse, "-".join(self.scales.values()))
+        """ Just cat everything in the max scale defined by the CIM code. """
+        return "%s%s-%s-%s" % (self.hemisphere, self.zone, self.fuse, "-".join(self.scales))
+    scalesindex = {"500k": 0, "250k": 1, "100k": 2, "50k": 3, "25k": 4}
+    def __get__(self, scale):
+        return self.scales[0:scalesindex[scale] + 1]
             
             
