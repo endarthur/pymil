b071aaeb691e8b9cfd8512e73f12bdd79a2cfddb
Arthur Endlein <endarthur@gmail.com>
2012-06-27 17:16:02 -0300

    Just a bit more of cleaning up, thanks to the meeting. Now geany correctly recognize the class, wouldn't do before because of identation.

--- cim.py
+++ cim.py
@@ -12,10 +12,10 @@ class CIMcode(object):
         self.scalesindex = {"500k": 0, "250k": 1, "100k": 2, "50k": 3, "25k": 4}
         self.coordinates = (lat, lon) # Just might be useful to store a small coordinates tupple. Who knows?
         self.scales = [(('V', 'X'), ('Y', 'Z'))[int(lat % 4 / 2)][int(lon % 6 / 3)], # 2 by 3
-(('A', 'B'), ('C', 'D'))[int(lat % 2 / 1)][int(lon % 3 / 1.5)], # 1 by 1.5
-(('I', 'II', 'III'), ('IV', 'V', 'VI'))[int(lat % 1 / 0.5)][int(lon % 1.5 / 0.5)], # 0.5 by 0.5
-(('1', '2'), ('3', '4'))[int(lat % 0.5 / 0.25)][int(lon % 0.5 / 0.25)], # 0.25 by 0.25
-(('NO', 'NE'), ('SO', 'SE'))[int(lat % 0.25 / 0.125)][int( lon % 0.25 / 0.125)]] # 0.125 by 0.125
+        (('A', 'B'), ('C', 'D'))[int(lat % 2 / 1)][int(lon % 3 / 1.5)], # 1 by 1.5
+        (('I', 'II', 'III'), ('IV', 'V', 'VI'))[int(lat % 1 / 0.5)][int(lon % 1.5 / 0.5)], # 0.5 by 0.5
+        (('1', '2'), ('3', '4'))[int(lat % 0.5 / 0.25)][int(lon % 0.5 / 0.25)], # 0.25 by 0.25
+        (('NO', 'NE'), ('SO', 'SE'))[int(lat % 0.25 / 0.125)][int( lon % 0.25 / 0.125)]] # 0.125 by 0.125
         self.hemisphere = (lat > 0) and "N" or "S"
         self.zone = zone[int(abs(lat))/4]
         self.fuse = str((180 + int(lon))/6 + 2)
