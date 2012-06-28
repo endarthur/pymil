1eafad789074101ff263c965584b246d1bd244a9
Arthur Endlein <endarthur@gmail.com>
2012-06-27 21:33:23 -0300

    Made it GPL, basically.

--- cim.py
+++ cim.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+#==============================================================================#
+#      CIMcode - Open-source Carte internationale du Monde au Millionième      #
+#                               Scale Coder                                    #
+#                                                                              #
+#    Copyright (c)  2012 Arthur Endlein.                                       #
+#                                                                              #
+#                                                                              #
+#    This file is part of CIMcode.                                             #
+#                                                                              #
+#    CIMcode is free software: you can redistribute it and/or modify           #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    CIMcode is distributed in the hope that it will be useful,                #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with CIMcode.  If not, see <http://www.gnu.org/licenses/>.          #
+#                                                                              #
+#                                                                              #
+#==============================================================================#
+
 # A International World Map encoder - from geographic coordinates
 # May someday accept UTM as well, but tonight it will be simpler
 # OO mode, hopefully. Maybe.
